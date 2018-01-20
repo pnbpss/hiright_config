b0355d5d30316943a78e853b200af65e978c25ca
Panu Boonpromsook <pnbpss@gmail.com>
2018-01-20 14:47:29 +0700

    second commit

--- install.bat
+++ install.bat
@@ -7,13 +7,14 @@ if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
 set dtStamp=%dtStamp: =_%
 
 REM script by panu boonpromsook copy right Tangjai Yontrakarn
-ren "C:\Program Files\SQLLIB\DB2\SQLDBDIR"  "C:\Program Files\SQLLIB\DB2\SQLDBDIR_%dtStamp%"
-ren "C:\Program Files\SQLLIB\DB2\SQLNODIR"  "C:\Program Files\SQLLIB\DB2\SQLDBDIR_%dtStamp%"
+ren "C:\Program Files\SQLLIB\DB2\SQLDBDIR"  "SQLDBDIR_%dtStamp%"
+ren "C:\Program Files\SQLLIB\DB2\SQLNODIR"  "SQLDBDIR_%dtStamp%"
+pause
 
 mkdir "C:\Program Files\SQLLIB\DB2\SQLDBDIR"
-copy SQLDBDIR\*.* "C:\Program Files\SQLLIB\DB2\SQLDBDIR"
+copy /y SQLDBDIR\*.* "C:\Program Files\SQLLIB\DB2\SQLDBDIR"
 
 mkdir "C:\Program Files\SQLLIB\DB2\SQLNODIR"
-copy SQLNODIR\*.* "C:\Program Files\SQLLIB\DB2\SQLNODIR"
+copy /y SQLNODIR\*.* "C:\Program Files\SQLLIB\DB2\SQLNODIR"
 
 pause
---
+++ สลับฐาน/02_ภาษี_เก่า.bat
@@ -0,0 +1,23 @@
+echo ; Comment lines start with a semi-colon. > "C:\Program Files\SQLLIB\db2cli.ini"
+echo ;>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo [tstcli1x]>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo uid=userid>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo pwd=password>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo autocommit=0>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo TableType="'TABLE','VIEW','SYSTEM TABLE'">> "C:\Program Files\SQLLIB\db2cli.ini"
+echo ;>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo [tstcli2x]>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo ;Assuming dbalias2 is a database in DB2 for MVS.>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo SchemaList="'OWNER1','OWNER2',CURRENT SQLID">> "C:\Program Files\SQLLIB\db2cli.ini"
+echo ;>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo [MyVeryLongDBALIASName]>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo dbalias=dbalias3>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo SysSchema=MYSCHEMA>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo ;>> "C:\Program Files\SQLLIB\db2cli.ini"
+
+echo [HISERIES]>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo PWD=InCome2K>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo UID=HIRIGHT>> "C:\Program Files\SQLLIB\db2cli.ini"
+echo DBALIAS=TAXOLD>> "C:\Program Files\SQLLIB\db2cli.ini"
+
+echo OK!!
